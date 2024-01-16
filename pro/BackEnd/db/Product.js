536ad189f4495f86bc88aa64cbdcb4329b638edd
twinkle <twinkle.parmar@archesoftronix.com>
2024-01-16 12:58:11 +0530

    node js basic, basic crud operations and practice projects

--- pro/BackEnd/db/Product.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const productSchema = new mongoose.Schema({
-  name: String,
-  price: String,
-});
-
-module.exports = mongoose.model('products', productSchema);
